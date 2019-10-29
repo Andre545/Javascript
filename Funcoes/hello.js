2f9a9c7207489cefe9a49e0d98b74a9cee82b2f5
Andre545 <57118460+Andre545@users.noreply.github.com>
2019-10-28 22:27:48 -0300

    Adicionei 3 pastas da aula 1

---
+++ Funcoes/hello.js
@@ -0,0 +1,10 @@
+/**
+ * Eemplo de uma função simples
+ * @author André Barros
+ */
+
+ function helloWorld(){
+     let nome
+     nome = prompt ("Qual é o seu nome ?")
+     alert ("Hello " + nome)
+ }
---
+++ vincularJS/documento.js
@@ -0,0 +1,7 @@
+/**
+ * Vinculando um documento JS no html
+ * Exemplo de vinculação de um documento JS no html
+ * @author André Barros
+ */
+
+ document.write("<h2>" + "Hello World" + "<h2>")
